18e83eced728e2ff2ab9a859fffb76763b474b4c
Pavel Markeloff <pmarkelo77@gmail.com>
2021-03-06 10:45:15 +0400

    Убрал арифметику (думаю как реализовать). Добавил интерфейс дробных чисел

--- CurseWork/CurseWork/mainwindow.cpp
+++ CurseWork/CurseWork/mainwindow.cpp
@@ -3,7 +3,7 @@
 
 double numFirst = 0, numSecond = 0;
 
-Operations ops = None;
+bool flagInputDig = false; // while label.text = 0 => flag = false;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -35,6 +35,18 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->pushButton_z8, SIGNAL(clicked()), this, SLOT(z_numbers()));
     connect(ui->pushButton_z9, SIGNAL(clicked()), this, SLOT(z_numbers()));
 
+    // Обрабатываем нажатие дробных чисел (числитель)
+    connect(ui->pushButton_ch0, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch1, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch2, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch3, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch4, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch5, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch6, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch7, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch8, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+    connect(ui->pushButton_ch9, SIGNAL(clicked()), this, SLOT(ch_numbers()));
+
     // Обрабатывам арифметические операции
     connect(ui->pushButton_precent, SIGNAL(clicked()), this, SLOT(operations()));
     connect(ui->pushButton_plus_minus, SIGNAL(clicked()), this, SLOT(operations()));
@@ -57,9 +69,12 @@ MainWindow::~MainWindow()
 
 // Обратабываем целые числа
 void MainWindow::digits_numbers() {
-    ui->label->setText(""); // Очистим сразу поле. Т.к при вводе числа и выбора операции нам нужно видеть старое число.
+    //ui->label->setText(""); // Очистим сразу поле. Т.к при вводе числа и выбора операции нам нужно видеть старое число.
                             // После выбора операции вводим новое число и тогда label чиститься.
     QPushButton *button = (QPushButton *)sender(); // Обрабатываем нажатие кннопки.
+
+    if (ui->label->text() != "0") flagInputDig = true;
+    else flagInputDig = false;
 
     double all_numbers;
     QString new_label;
@@ -73,6 +88,39 @@ void MainWindow::digits_numbers() {
 // Обратабываем дробные числа (знаменатель)
 void MainWindow::z_numbers() {
 
+    QPushButton *button = (QPushButton *)sender(); // Обрабатываем нажатие кннопки.
+
+    ui->label->setGeometry(10, 0, 451, 71);
+    if (flagInputDig == false) ui->label->setText("");
+    ui->labelZ->setEnabled(true);
+    ui->labelCH->setEnabled(true);
+
+    double all_numbers;
+    QString new_label;
+
+    all_numbers = (ui->labelZ->text() + button->text()).toDouble();
+    new_label = QString::number(all_numbers, 'g', 5);
+
+    ui->labelZ->setText(new_label);
+}
+
+// Обратабываем дробные числа (числитель)
+void MainWindow::ch_numbers() {
+
+    QPushButton *button = (QPushButton *)sender(); // Обрабатываем нажатие кннопки.
+
+    ui->label->setGeometry(10, 0, 451, 71);
+    if (flagInputDig == false) ui->label->setText("");
+    ui->labelZ->setEnabled(true);
+    ui->labelCH->setEnabled(true);
+
+    double all_numbers;
+    QString new_label;
+
+    all_numbers = (ui->labelCH->text() + button->text()).toDouble();
+    new_label = QString::number(all_numbers, 'g', 5);
+
+    ui->labelCH->setText(new_label);
 }
 
 // Кнопка точки :)
@@ -104,89 +152,23 @@ void MainWindow::operations() {
     }
 }
 
-// Обработчки операций.
-void MainWindow::math_operations() {
-    QPushButton *button = (QPushButton *)sender(); // Обработаем кнопку.
-
-    QString new_label;
-
-    numSecond = ui->label->text().toDouble(); // Возьмем второе число после выбора операции.
-
-    if (numFirst == 0) // Если у нас первое число равно 0, то возьмем для него след.
-        numFirst = ui->label->text().toDouble();
-    else { // Иначе нам надо запонить выбор. Пример: 5 + 5 (в памяти это равно 10, но мы не видим),
-           //                                (выберем след операцию) - (выведется 10 и выберем число) 4 = 6.
-        double lNum = 0;
-        button->setChecked(true); // true на ту кнопку, которая нажата.
-        if(ui->pushButton_plus->isChecked()) {
-            lNum = numFirst + numSecond;
-            ui->pushButton_plus->setChecked(false);
-        }
-        else if(ui->pushButton_minus->isChecked()) {
-            lNum = numFirst - numSecond;
-            ui->pushButton_minus->setChecked(false);
-        }
-        else if(ui->pushButton_mul->isChecked()) {
-            lNum = numFirst * numSecond;
-            ui->pushButton_mul->setChecked(false);
-        }
-        else if(ui->pushButton_div->isChecked()) {
-            if (numSecond == 0) ui->label->setText("0");
-            else {
-                lNum = numFirst / numSecond;
-                ui->pushButton_div->setChecked(false);
-            }
-        }
-        new_label = QString::number(lNum, 'g', 10);
-        numFirst = lNum;
-        ui->label->setText(new_label);
-    }
-    button->setChecked(true);
-}
-
-// Арифметика.
-void MainWindow::on_pushButton_equally_clicked()
-{
-    double lNum = 0;
-    QString new_label;
-    numSecond = 0;
-    numSecond = ui->label->text().toDouble();
-
-    if(ui->pushButton_plus->isChecked()) {
-        lNum = numFirst + numSecond;
-        new_label = QString::number(lNum, 'g', 10);
-        ui->pushButton_plus->setChecked(false);
-    }
-    else if (ui->pushButton_minus->isChecked()) {
-        lNum = numFirst - numSecond;
-        ui->pushButton_minus->setChecked(false);
-    }
-    else if (ui->pushButton_mul->isChecked()) {
-        lNum = numFirst * numSecond;
-        ui->pushButton_mul->setChecked(false);
-    }
-    else if (ui->pushButton_div->isChecked()) {
-        if (numSecond == 0) ui->label->setText("0");
-        else {
-            lNum = numFirst / numSecond;
-            ui->pushButton_div->setChecked(false);
-        }
-    }
-
-    new_label = QString::number(lNum, 'g', 10);
-    ui->label->setText(new_label);
-    numFirst = 0;
-}
-
 // Очистка всего.
 void MainWindow::on_pushButton_AC_clicked()
 {
+    flagInputDig = false;
+
     ui->pushButton_plus->setChecked(false);
     ui->pushButton_minus->setChecked(false);
     ui->pushButton_mul->setChecked(false);
     ui->pushButton_div->setChecked(false);
 
+    ui->label->setGeometry(10, 0, 531, 71);
+    ui->labelZ->setEnabled(false);
+    ui->labelCH->setEnabled(false);
+
     ui->label->setText("0");
+    ui->labelZ->setText("0");
+    ui->labelCH->setText("0");
 
     numFirst = 0;
     numSecond = 0;
@@ -197,6 +179,94 @@ void MainWindow::on_pushButton_C_clicked()
 {
     QString text = ui->label->text();
     text.chop(1);
-    if (text.isEmpty()) text = "0";
+    if (text.isEmpty()) {
+        text = "0";
+        flagInputDig = false;
+    }
     ui->label->setText(text);
+}
+
+// Очистка лишь символа. (числитель)
+void MainWindow::on_pushButton_chC_clicked()
+{
+    QString textN = ui->label->text();
+    QString text = ui->labelCH->text();
+    QString text2 = ui->labelZ->text();
+    text.chop(1);
+    if (text.isEmpty()) {
+        if (ui->labelZ->text() == "0" && ui->labelCH->text() == "0") {
+            ui->label->setGeometry(10, 0, 531, 71);
+            if (textN.isEmpty()) ui->label->setText("0");
+            else ui->label->setText(textN);
+            ui->labelZ->setEnabled(false);
+            ui->labelCH->setEnabled(false);
+        }
+        text = "0";
+    }
+    ui->labelCH->setText(text);
+}
+
+// Очистка лишь символа. (знаменатель)
+void MainWindow::on_pushButton_zC_clicked()
+{
+    QString textN = ui->label->text();
+    QString text = ui->labelZ->text();
+    QString text2 = ui->labelCH->text();
+    text.chop(1);
+    if (text.isEmpty()) {
+        if (ui->labelZ->text() == "0" && ui->labelCH->text() == "0") {
+            ui->label->setGeometry(10, 0, 531, 71);
+            if (textN.isEmpty()) ui->label->setText("0");
+            else ui->label->setText(textN);
+            ui->labelZ->setEnabled(false);
+            ui->labelCH->setEnabled(false);
+        }
+        text = "0";
+    }
+    ui->labelZ->setText(text);
+}
+
+// Кнопка точки. (знаменатель)
+void MainWindow::on_pushButton_dotZ_clicked()
+{
+    ui->label->setGeometry(10, 0, 451, 71);
+    ui->labelZ->setEnabled(true);
+    ui->labelCH->setEnabled(true);
+    if(!(ui->labelZ->text().contains('.'))) // Если точка уже есть в тексте, то зачем её вводить снова?
+        ui->labelZ->setText(ui->labelZ->text() + ".");
+}
+
+// Кнопка точки. (числитель)
+void MainWindow::on_pushButton_dotCH_clicked()
+{
+    ui->label->setGeometry(10, 0, 451, 71);
+    ui->labelZ->setEnabled(true);
+    ui->labelCH->setEnabled(true);
+    if(!(ui->labelCH->text().contains('.'))) // Если точка уже есть в тексте, то зачем её вводить снова?
+        ui->labelCH->setText(ui->labelCH->text() + ".");
+}
+
+// Инструкция.
+void MainWindow::on_action_triggered()
+{
+
+}
+
+// Очистка всего через меню.
+void MainWindow::on_action_AC_triggered()
+{
+    on_pushButton_AC_clicked();
+}
+
+void MainWindow::on_action_Exit_triggered()
+{
+    qApp->quit();
+}
+
+void MainWindow::on_action_Author_triggered()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Автор - Булатов Павел\nОбратная связь: example@text.com\nGitHub: github.com/ofu-hub");
+    msgBox.setWindowTitle("Автор");
+    msgBox.exec();
 }
--- CurseWork/CurseWork/mainwindow.h
+++ CurseWork/CurseWork/mainwindow.h
@@ -2,18 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
-enum Operations {
-    None = -1,
-    Plus = 0,
-    Minus = 1,
-    Div = 2,
-    Mul = 3
-};
 
 class MainWindow : public QMainWindow
 {
@@ -30,10 +23,17 @@ private slots:
     void digits_numbers();
     void on_pushButton_dot_clicked();
     void operations();
-    void math_operations();
-    void on_pushButton_equally_clicked();
     void on_pushButton_AC_clicked();
     void on_pushButton_C_clicked();
     void z_numbers();
+    void ch_numbers();
+    void on_pushButton_chC_clicked();
+    void on_pushButton_zC_clicked();
+    void on_pushButton_dotZ_clicked();
+    void on_pushButton_dotCH_clicked();
+    void on_action_triggered();
+    void on_action_AC_triggered();
+    void on_action_Exit_triggered();
+    void on_action_Author_triggered();
 };
 #endif // MAINWINDOW_H
